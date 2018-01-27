68ece30fde438adf19372d5addc11da696cb57a9
Andrew Rynhard <andrew@andrewrynhard.com>
2018-01-27 10:19:50 -0800

    feat(*): display current terraform workspace

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -42,116 +42,121 @@ __pure_set_default pure_user_host_location 0
 __pure_set_default pure_command_max_exec_time 5
 
 function fish_prompt
-  # Save previous exit code
-  set -l exit_code $status
+    # Save previous exit code
+    set -l exit_code $status
 
-  # Set default color symbol to green meaning it's all good!
-  set -l color_symbol $pure_color_green
+    # Set default color symbol to green meaning it's all good!
+    set -l color_symbol $pure_color_green
 
-  # Template
+    # Template
 
-  set -l user_and_host ""
-  set -l current_folder (__parse_current_folder)
-  set -l git_branch_name ""
-  set -l git_dirty ""
-  set -l git_arrows ""
-  set -l command_duration ""
-  set -l prompt ""
-  set -l kube_context ""
+    set -l user_and_host ""
+    set -l current_folder (__parse_current_folder)
+    set -l git_branch_name ""
+    set -l git_dirty ""
+    set -l git_arrows ""
+    set -l command_duration ""
+    set -l prompt ""
+    set -l terraform_workspace ""
+    set -l kube_context ""
 
-  # Check if user is in an SSH session
-  if [ "$SSH_CONNECTION" != "" ]
-    set -l host (hostname -s)
-    set -l user (whoami)
+    # Check if user is in an SSH session
+    if [ "$SSH_CONNECTION" != "" ]
+        set -l host (hostname -s)
+        set -l user (whoami)
 
-    if [ "$user" = "root" ]
-      set user "$pure_root_color$user"
-    else
-      set user "$pure_username_color$user"
+        if [ "$user" = "root" ]
+            set user "$pure_root_color$user"
+        else
+            set user "$pure_username_color$user"
+        end
+
+        # Format user and host part of prompt
+        set user_and_host "$user$pure_color_gray@$pure_host_color$host$pure_color_normal "
     end
 
-    # Format user and host part of prompt
-    set user_and_host "$user$pure_color_gray@$pure_host_color$host$pure_color_normal "
-  end
-
-  if test $pure_user_host_location -eq 1
-    set prompt $prompt $user_and_host
-  end
-
-  # Format current folder on prompt output
-  set prompt $prompt "$pure_color_blue$current_folder$pure_color_normal "
-
-  # Handle previous failed command
-  if test $exit_code -ne 0
-    # Symbol color is red when previous command fails
-    set color_symbol $pure_color_red
-  end
-
-  # Exit with code 1 if git is not available
-  if not which git >/dev/null
-    return 1
-  end
-
-  # Check if is on a Git repository
-  set -l is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
-
-  if test -n "$is_git_repository"
-    set git_branch_name (__parse_git_branch)
-
-    # Check if there are files to commit
-    set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
-
-    if test -n "$is_git_dirty"
-      set git_dirty $pure_symbol_git_dirty
+    if test $pure_user_host_location -eq 1
+        set prompt $prompt $user_and_host
     end
 
-    # Check if there is an upstream configured
-    command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1; and set -l has_upstream
-    if set -q has_upstream
-      set -l git_status (command git rev-list --left-right --count 'HEAD...@{upstream}' | sed "s/[[:blank:]]/ /" ^/dev/null)
+    # Format current folder on prompt output
+    set prompt $prompt "$pure_color_blue$current_folder$pure_color_normal "
 
-      # Resolve Git arrows by treating `git_status` as an array
-      set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
-      set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
-
-    # If arrow is not "0", it means it's dirty
-      if test $git_arrow_left != 0
-        set git_arrows " $pure_symbol_git_up_arrow"
-      end
-
-      if test $git_arrow_right != 0
-        set git_arrows " $git_arrows$pure_symbol_git_down_arrow"
-      end
+    # Handle previous failed command
+    if test $exit_code -ne 0
+        # Symbol color is red when previous command fails
+        set color_symbol $pure_color_red
     end
 
-    # Format Git prompt output
-    set prompt $prompt "$pure_color_gray$git_branch_name$git_dirty$pure_color_normal$pure_color_cyan$git_arrows$pure_color_normal "
-  end
+    # Exit with code 1 if git is not available
+    if not which git >/dev/null
+        return 1
+    end
 
-  if test $pure_user_host_location -ne 1
-    set prompt $prompt $user_and_host
-  end
+    # Check if is on a Git repository
+    set -l is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
 
-  # Prompt command execution duration
-  if test -n "$CMD_DURATION"
-    set command_duration (__format_time $CMD_DURATION $pure_command_max_exec_time)
-  end
-  set prompt $prompt "$pure_color_yellow$pure_color_normal$pure_symbol_vertical_bar$pure_color_yellow "
+    if test -n "$is_git_repository"
+        set git_branch_name (__parse_git_branch)
 
-  # Show python virtualenv name (if activated)
-  if test -n "$VIRTUAL_ENV"
-    set prompt $prompt $pure_color_gray(basename "$VIRTUAL_ENV")"$pure_color_normal "
-  end
+        # Check if there are files to commit
+        set -l is_git_dirty (command git status --porcelain --ignore-submodules ^/dev/null)
 
-  if test -n "kubectl"
-    set kube_context (kubectl config current-context)
-  end
+        if test -n "$is_git_dirty"
+            set git_dirty $pure_symbol_git_dirty
+        end
 
-  set prompt $prompt "$kube_context $pure_color_yellow$command_duration$pure_color_normal"
+        # Check if there is an upstream configured
+        command git rev-parse --abbrev-ref '@{upstream}' >/dev/null ^&1
+        and set -l has_upstream
+        if set -q has_upstream
+            set -l git_status (command git rev-list --left-right --count 'HEAD...@{upstream}' | sed "s/[[:blank:]]/ /" ^/dev/null)
 
-  set prompt $prompt "$color_symbol$pure_symbol_prompt$pure_color_normal "
+            # Resolve Git arrows by treating `git_status` as an array
+            set -l git_arrow_left (command echo $git_status | cut -c 1 ^/dev/null)
+            set -l git_arrow_right (command echo $git_status | cut -c 3 ^/dev/null)
 
-  echo -e -s $prompt
+            # If arrow is not "0", it means it's dirty
+            if test $git_arrow_left != 0
+                set git_arrows " $pure_symbol_git_up_arrow"
+            end
 
-  set __pure_fresh_session 0
+            if test $git_arrow_right != 0
+                set git_arrows " $git_arrows$pure_symbol_git_down_arrow"
+            end
+        end
+
+        # Format Git prompt output
+        set prompt $prompt "$pure_color_gray$git_branch_name$git_dirty$pure_color_normal$pure_color_cyan$git_arrows$pure_color_normal "
+    end
+
+    if test $pure_user_host_location -ne 1
+        set prompt $prompt $user_and_host
+    end
+
+    # Prompt command execution duration
+    if test -n "$CMD_DURATION"
+        set command_duration (__format_time $CMD_DURATION $pure_command_max_exec_time)
+    end
+    set prompt $prompt "$pure_color_yellow$pure_color_normal$pure_symbol_vertical_bar$pure_color_yellow "
+
+    if test -n "terraform"
+        set terraform_workspace (terraform workspace show)
+    end
+
+    set prompt $prompt "$pure_color_cyan$terraform_workspace$pure_color_normal "
+
+    set prompt $prompt "$pure_color_yellow$pure_color_normal$pure_symbol_vertical_bar$pure_color_yellow "
+
+    if test -n "kubectl"
+        set kube_context (kubectl config current-context)
+    end
+
+    set prompt $prompt "$pure_color_cyan$kube_context$pure_color_normal $pure_color_yellow$command_duration$pure_color_normal"
+
+    set prompt $prompt "$color_symbol$pure_symbol_prompt$pure_color_normal "
+
+    echo -e -s $prompt
+
+    set __pure_fresh_session 0
 end
